6577b3b6a91e20aeff26ec05f74784e5eb782e4e
Raskerov <robertsikora1997@gmail.com>
2022-01-17 20:29:30 +0100

    Fix HOST, bump gemfile

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  Rails.application.routes.default_url_options[:host] = ENV['HOST']
+  Rails.application.routes.default_url_options[:host] = ENV['EMAIL_HOST']
   prepend_view_path 'app/views/mailers'
 
   default from: 'no-reply@horario'
